d131b01043f046d3a4f125707996f9a771056e89
Marcelo Serpa <boss@fullofcaffeine.com>
2020-10-30 16:54:00 -0600

    Code-removing therapy
    Remove code that's explicitly commented out in the coblocks-builder files.

--- .dev/config/webpack.config.js
+++ .dev/config/webpack.config.js
@@ -17,13 +17,9 @@ module.exports = {
 		'coblocks-editor': path.resolve( process.cwd(), 'src/styles/editor.scss' ),
 		'coblocks-style': path.resolve( process.cwd(), 'src/styles/style.scss' ),
 
-		'js/coblocks-accordion-polyfill': path.resolve( process.cwd(), 'src/js/coblocks-accordion-polyfill.js' ),
 		'js/coblocks-accordion-carousel': path.resolve( process.cwd(), 'src/js/coblocks-accordion-carousel.js' ),
 		'js/coblocks-datepicker': path.resolve( process.cwd(), 'src/js/coblocks-datepicker.js' ),
 		'js/coblocks-events': path.resolve( process.cwd(), 'src/js/coblocks-events.js' ),
-		'js/coblocks-fromEntries': path.resolve( process.cwd(), 'src/js/coblocks-fromEntries.js' ),
-		'js/coblocks-google-maps': path.resolve( process.cwd(), 'src/js/coblocks-google-maps.js' ),
-		'js/coblocks-google-recaptcha': path.resolve( process.cwd(), 'src/js/coblocks-google-recaptcha.js' ),
 		'js/coblocks-lightbox': path.resolve( process.cwd(), 'src/js/coblocks-lightbox.js' ),
 		'js/coblocks-masonry': path.resolve( process.cwd(), 'src/js/coblocks-masonry.js' ),
 		'js/coblocks-slick-initializer': path.resolve( process.cwd(), 'src/js/coblocks-slick-initializer.js' ),
--- src/js/coblocks-fromEntries.js
+++
@@ -1,7 +0,0 @@
-// Helper for Object.fromEntries for IE11, Edge, and Opera.
-export default function fromEntries( iterable ) {
-	return [ ...iterable ].reduce(
-		( obj, { 0: key, 1: val } ) => Object.assign( obj, { [ key ]: val } ),
-		{}
-	);
-}
